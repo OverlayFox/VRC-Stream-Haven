86b43f6798af229084ddef13cd0251c4baf37b9b
Simon Koester <s.koester@clipmyhorse.tv>
2024-05-18 20:56:16 +0200

    test

--- main.go
+++ main.go
@@ -10,26 +10,26 @@ import (
 func main() {
 	lib.InitialiseConfig()
 
-	//rtspHandler := &servers.RtspServerHandler{}
-	//rtspHandler.Server = &gortsplib.Server{
-	//	Handler:     rtspHandler,
-	//	RTSPAddress: ":8554",
-	//}
-
-	rtspNodeHandler := &servers.RtspNodeServerHandler{}
-	rtspNodeHandler.Server = &gortsplib.Server{
-		Handler:     rtspNodeHandler,
+	rtspHandler := &servers.RtspServerHandler{}
+	rtspHandler.Server = &gortsplib.Server{
+		Handler:     rtspHandler,
 		RTSPAddress: ":8554",
 	}
 
-	//log.Println("Server is ready.....")
-	//panic(rtspHandler.Server.StartAndWait())
+	//rtspNodeHandler := &servers.RtspNodeServerHandler{}
+	//rtspNodeHandler.Server = &gortsplib.Server{
+	//	Handler:     rtspNodeHandler,
+	//	RTSPAddress: ":8554",
+	//}
 
-	log.Println("Node Server is ready.....")
-	go func() {
-		lib.RelayHlsToRtsp("http://10.58.97.100/tmp/streams/playlist.m3u8", "rtsp://localhost:8554/ingest/channel")
-	}()
-	panic(rtspNodeHandler.Server.StartAndWait())
+	log.Println("Server is ready.....")
+	panic(rtspHandler.Server.StartAndWait())
+
+	//log.Println("Node Server is ready.....")
+	//go func() {
+	//	lib.RelayHlsToRtsp("http://10.58.97.100/tmp/streams/playlist.m3u8", "rtsp://localhost:8554/ingest/channel")
+	//}()
+	//panic(rtspNodeHandler.Server.StartAndWait())
 
 	//flag.Parse()
 	//go servers.StartRtmpServer()
